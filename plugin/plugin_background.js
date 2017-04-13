51a6a09abac66e50e9aab9e0487b4eba12e2af21
Will Blaschko <willblas@amazon.com>
2017-04-12 17:05:03 -0700

    Revert "initial commit, add plugin from other repo"
    This reverts commit 14f497bbeaca9e0f55281347d599272713dc5176.

--- plugin/plugin_background.js
+++
@@ -1,8 +0,0 @@
-let loaded = false;
-chrome.tabs.onUpdated.addListener(function(tabId, info) {
-    if (info.status == "complete") {
-        chrome.tabs.executeScript(tabId, { file: "plugin_inject.js" });
-        loaded = true;
-        
-    }
-});
