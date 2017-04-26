dda6972e193ea69d020dc5c96b710a2861fba538
Will Blaschko <willblas@amazon.com>
2017-04-26 12:24:44 -0700

    add commands to generate greasemonkey script and add greasemonkey header

---
+++ plugin/greasemonkey_header.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Dev Portal Validator
+// @namespace    http://developer.amazon.com/
+// @version      0.1
+// @description  Run automatic SLU validation from the developer portal language model page.
+// @author       willblas@amazon.com
+// @grant        GM_xmlhttpRequest
+// @grant        GM_addStyle
+// @connect      *
+// @include      http://developer.amazon.com/*
+// @include      https://developer.amazon.com/*
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+})();
